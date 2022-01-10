42fc54016beeea24b8e063ef3afd651732cb3b15
chengjiajia <chengjiajia@haocang.com>
2022-01-10 17:59:58 +0800

    状态模式

---
+++ State/S.js
@@ -0,0 +1,21 @@
+var marry = {
+    state: {
+        jump: function() {
+            console.log("jump")
+        },
+        move: function() {
+            console.log("move")
+        },
+        shoot: function() {
+            console.log("shoot")
+        },
+        say: function() {
+            console.log("say")
+        }
+    },
+    action: {
+        change: function() {
+            
+        }
+    }
+}
